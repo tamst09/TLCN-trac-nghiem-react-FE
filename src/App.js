d33936d9dccca56fd08421e49e7961d6e2eef476
Tam Tran <tamst09@gmail.com>
2021-03-05 18:20:00 +0700

    create-home-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect, Link,
+  useRouteMatch, useParams
+} from "react-router-dom";
+// Import Pages
+import { Homepage } from "./pages/Homepage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/login' component={Loginpage} />
+        <Route path='/notfound' component={Notfoundpage} />
+        <Redirect to='/notfound' />
+      </Switch>
+    </Router>
+  );
+}
+const Loginpage = () => {
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <h3>Please login to continue...</h3>
+      <Link to={`${url}/loginok`}>Login</Link>
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+function Topic() {
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
     </div>
   );
 }
-
+const Notfoundpage = () => {
+  return (
+    <h3>Page not found...</h3>
+  )
+}
 export default App;
