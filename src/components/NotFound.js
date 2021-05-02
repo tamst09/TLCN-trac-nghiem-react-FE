c70dbffa92f79d12ddbcf4ad36f804c7475236aa
Tam Tran <tamst09@gmail.com>
2021-05-02 21:15:06 +0700

    structure

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>Opps!! Page not found...</h3>
+        </div>
+    )
+}
+
+export default NotFound;
